c715e4f06c85f79bfdedec247a328913c1ebbefc
FilipaILoureiro <filipairinaloureiro@gmail.com>
2025-09-01 14:19:33 +0100

    removed pdf button from logs

--- Forms Login e Registo/LogsList.Designer.cs
+++ Forms Login e Registo/LogsList.Designer.cs
@@ -31,28 +31,25 @@
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogsList));
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogsList));
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             dgvLogs = new Guna.UI2.WinForms.Guna2DataGridView();
             guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            btnPDF = new Guna.UI2.WinForms.Guna2Button();
             guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
             lblDataFim = new Guna.UI2.WinForms.Guna2HtmlLabel();
             lblDataInicio = new Guna.UI2.WinForms.Guna2HtmlLabel();
@@ -127,42 +124,14 @@
             // 
             // guna2Panel1
             // 
-            guna2Panel1.Controls.Add(btnPDF);
             guna2Panel1.Controls.Add(guna2Panel2);
-            guna2Panel1.CustomizableEdges = customizableEdges17;
+            guna2Panel1.CustomizableEdges = customizableEdges15;
             guna2Panel1.Dock = DockStyle.Top;
             guna2Panel1.Location = new Point(0, 0);
             guna2Panel1.Name = "guna2Panel1";
-            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges16;
             guna2Panel1.Size = new Size(1169, 223);
             guna2Panel1.TabIndex = 3;
-            // 
-            // btnPDF
-            // 
-            btnPDF.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnPDF.BorderRadius = 10;
-            btnPDF.CustomizableEdges = customizableEdges1;
-            btnPDF.DisabledState.BorderColor = Color.DarkGray;
-            btnPDF.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnPDF.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnPDF.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnPDF.FillColor = Color.FromArgb(139, 194, 64);
-            btnPDF.FocusedColor = Color.FromArgb(97, 135, 44);
-            btnPDF.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
-            btnPDF.ForeColor = Color.White;
-            btnPDF.HoverState.FillColor = Color.FromArgb(97, 135, 44);
-            btnPDF.Image = (Image)resources.GetObject("btnPDF.Image");
-            btnPDF.ImageAlign = HorizontalAlignment.Left;
-            btnPDF.ImageOffset = new Point(5, 0);
-            btnPDF.ImageSize = new Size(40, 40);
-            btnPDF.Location = new Point(940, 73);
-            btnPDF.Name = "btnPDF";
-            btnPDF.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            btnPDF.Size = new Size(207, 83);
-            btnPDF.TabIndex = 2;
-            btnPDF.Text = "Gerar PDF";
-            btnPDF.TextAlign = HorizontalAlignment.Left;
-            btnPDF.TextOffset = new Point(2, 0);
             // 
             // guna2Panel2
             // 
@@ -180,11 +149,11 @@
             guna2Panel2.Controls.Add(btnLimpar);
             guna2Panel2.Controls.Add(cbSetor);
             guna2Panel2.Controls.Add(panelFiltrosTitle);
-            guna2Panel2.CustomizableEdges = customizableEdges15;
+            guna2Panel2.CustomizableEdges = customizableEdges13;
             guna2Panel2.Location = new Point(12, 23);
             guna2Panel2.Name = "guna2Panel2";
-            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges16;
-            guna2Panel2.Size = new Size(904, 174);
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            guna2Panel2.Size = new Size(1145, 174);
             guna2Panel2.TabIndex = 0;
             // 
             // lblDataFim
@@ -193,7 +162,7 @@
             lblDataFim.BackColor = Color.Transparent;
             lblDataFim.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblDataFim.ForeColor = Color.FromArgb(64, 64, 64);
-            lblDataFim.Location = new Point(695, 8);
+            lblDataFim.Location = new Point(936, 8);
             lblDataFim.Name = "lblDataFim";
             lblDataFim.Size = new Size(36, 30);
             lblDataFim.TabIndex = 30;
@@ -205,7 +174,7 @@
             lblDataInicio.BackColor = Color.Transparent;
             lblDataInicio.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblDataInicio.ForeColor = Color.FromArgb(64, 64, 64);
-            lblDataInicio.Location = new Point(474, 8);
+            lblDataInicio.Location = new Point(715, 8);
             lblDataInicio.Name = "lblDataInicio";
             lblDataInicio.Size = new Size(29, 30);
             lblDataInicio.TabIndex = 29;
@@ -217,7 +186,7 @@
             guna2TextBox1.BorderColor = Color.FromArgb(97, 135, 44);
             guna2TextBox1.BorderRadius = 10;
             guna2TextBox1.BorderThickness = 2;
-            guna2TextBox1.CustomizableEdges = customizableEdges3;
+            guna2TextBox1.CustomizableEdges = customizableEdges1;
             guna2TextBox1.DefaultText = "";
             guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
             guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
@@ -233,8 +202,8 @@
             guna2TextBox1.PlaceholderForeColor = Color.Gray;
             guna2TextBox1.PlaceholderText = "ID ou Nome Funcionário";
             guna2TextBox1.SelectedText = "";
-            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges4;
-            guna2TextBox1.Size = new Size(532, 47);
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2TextBox1.Size = new Size(773, 47);
             guna2TextBox1.TabIndex = 28;
             // 
             // dtpFim
@@ -242,17 +211,17 @@
             dtpFim.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             dtpFim.BorderRadius = 10;
             dtpFim.Checked = true;
-            dtpFim.CustomizableEdges = customizableEdges5;
+            dtpFim.CustomizableEdges = customizableEdges3;
             dtpFim.FillColor = Color.FromArgb(139, 194, 64);
             dtpFim.FocusedColor = Color.FromArgb(97, 135, 44);
             dtpFim.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             dtpFim.ForeColor = Color.White;
             dtpFim.Format = DateTimePickerFormat.Short;
-            dtpFim.Location = new Point(685, 41);
+            dtpFim.Location = new Point(926, 41);
             dtpFim.MaxDate = new DateTime(9998, 12, 31, 0, 0, 0, 0);
             dtpFim.MinDate = new DateTime(1753, 1, 1, 0, 0, 0, 0);
             dtpFim.Name = "dtpFim";
-            dtpFim.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            dtpFim.ShadowDecoration.CustomizableEdges = customizableEdges4;
             dtpFim.Size = new Size(181, 36);
             dtpFim.TabIndex = 27;
             dtpFim.Value = new DateTime(2025, 7, 1, 11, 27, 45, 77);
@@ -262,17 +231,17 @@
             dtpInicio.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             dtpInicio.BorderRadius = 10;
             dtpInicio.Checked = true;
-            dtpInicio.CustomizableEdges = customizableEdges7;
+            dtpInicio.CustomizableEdges = customizableEdges5;
             dtpInicio.FillColor = Color.FromArgb(139, 194, 64);
             dtpInicio.FocusedColor = Color.FromArgb(97, 135, 44);
             dtpInicio.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             dtpInicio.ForeColor = Color.White;
             dtpInicio.Format = DateTimePickerFormat.Short;
-            dtpInicio.Location = new Point(465, 41);
+            dtpInicio.Location = new Point(706, 41);
             dtpInicio.MaxDate = new DateTime(9998, 12, 31, 0, 0, 0, 0);
             dtpInicio.MinDate = new DateTime(1753, 1, 1, 0, 0, 0, 0);
             dtpInicio.Name = "dtpInicio";
-            dtpInicio.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            dtpInicio.ShadowDecoration.CustomizableEdges = customizableEdges6;
             dtpInicio.Size = new Size(181, 36);
             dtpInicio.TabIndex = 26;
             dtpInicio.Value = new DateTime(2025, 7, 1, 11, 27, 45, 95);
@@ -292,7 +261,7 @@
             // 
             btnLimpar.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             btnLimpar.BorderRadius = 10;
-            btnLimpar.CustomizableEdges = customizableEdges9;
+            btnLimpar.CustomizableEdges = customizableEdges7;
             btnLimpar.DisabledState.BorderColor = Color.DarkGray;
             btnLimpar.DisabledState.CustomBorderColor = Color.DarkGray;
             btnLimpar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -303,9 +272,9 @@
             btnLimpar.ForeColor = Color.White;
             btnLimpar.HoverState.FillColor = Color.FromArgb(139, 194, 64);
             btnLimpar.Image = (Image)resources.GetObject("btnLimpar.Image");
-            btnLimpar.Location = new Point(756, 105);
+            btnLimpar.Location = new Point(997, 105);
             btnLimpar.Name = "btnLimpar";
-            btnLimpar.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            btnLimpar.ShadowDecoration.CustomizableEdges = customizableEdges8;
             btnLimpar.Size = new Size(110, 47);
             btnLimpar.TabIndex = 24;
             btnLimpar.Text = "Limpar";
@@ -317,7 +286,7 @@
             cbSetor.BorderColor = Color.FromArgb(97, 135, 44);
             cbSetor.BorderRadius = 10;
             cbSetor.BorderThickness = 2;
-            cbSetor.CustomizableEdges = customizableEdges11;
+            cbSetor.CustomizableEdges = customizableEdges9;
             cbSetor.DrawMode = DrawMode.OwnerDrawFixed;
             cbSetor.DropDownStyle = ComboBoxStyle.DropDownList;
             cbSetor.FocusedColor = Color.FromArgb(94, 148, 255);
@@ -327,8 +296,8 @@
             cbSetor.ItemHeight = 30;
             cbSetor.Location = new Point(208, 41);
             cbSetor.Name = "cbSetor";
-            cbSetor.ShadowDecoration.CustomizableEdges = customizableEdges12;
-            cbSetor.Size = new Size(218, 36);
+            cbSetor.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            cbSetor.Size = new Size(459, 36);
             cbSetor.TabIndex = 23;
             // 
             // panelFiltrosTitle
@@ -339,11 +308,11 @@
             panelFiltrosTitle.BorderRadius = 10;
             panelFiltrosTitle.Controls.Add(pictureBox1);
             panelFiltrosTitle.Controls.Add(lblFiltros);
-            panelFiltrosTitle.CustomizableEdges = customizableEdges13;
+            panelFiltrosTitle.CustomizableEdges = customizableEdges11;
             panelFiltrosTitle.Dock = DockStyle.Left;
             panelFiltrosTitle.Location = new Point(0, 0);
             panelFiltrosTitle.Name = "panelFiltrosTitle";
-            panelFiltrosTitle.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            panelFiltrosTitle.ShadowDecoration.CustomizableEdges = customizableEdges12;
             panelFiltrosTitle.Size = new Size(171, 174);
             panelFiltrosTitle.TabIndex = 22;
             // 
@@ -404,6 +373,5 @@
         private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
         private Guna.UI2.WinForms.Guna2DateTimePicker dtpFim;
         private Guna.UI2.WinForms.Guna2DateTimePicker dtpInicio;
-        private Guna.UI2.WinForms.Guna2Button btnPDF;
     }
 }
